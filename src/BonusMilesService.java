5df3802708b8869ac7ceec238c0d3b2b5be9dc52
leily <leily@example.com>
2023-09-17 16:28:00 +0300

    Ignoring files and reformate code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
-    public int calculate (int price) {
+    public int calculate(int price) {
         int result;
-        result = price/20;
+        result = price / 20;
         return result;
     }
 }
